3d52109d032f649aa164857b526e3132ec1d679b
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-07-16 13:46:01 -0300

    Adicionando enumeradores para perfil, status e prioridade

---
+++ src/main/java/org/example/domain/enums/Perfil.java
@@ -0,0 +1,39 @@
+package org.example.domain.enums;
+
+public enum Perfil {
+
+    //Enumerador com tipos de perfis que estarão disponíveis
+    ADMIN(0, "ROLE_ADMIN"), CLIENTE(1, "ROLE_CLIENTE"), TECNICO(2, "ROLE_TECNICO");
+
+    private Integer codigo;
+    private String descricao;
+
+    private Perfil(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    //Método irá verificar se o perfil informado existe e se existir irá retornar
+    public static Perfil toEnum(Integer cod) {
+        if(cod == null) {
+            return null;
+        }
+
+        for(Perfil perfil : Perfil.values()) {
+            if(cod.equals(perfil.getCodigo())) {
+                return perfil;
+            }
+        }
+
+        throw new IllegalArgumentException("Perfil Inválido!");
+    }
+}
+
