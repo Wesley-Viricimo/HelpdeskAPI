a37366d1eae6a1a4ae36adc477df9b98e4188b6b
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-08-20 19:55:40 -0300

    Adicionando lombok nas classes DTO

--- src/main/java/org/example/domain/dtos/ChamadoDTO.java
+++ src/main/java/org/example/domain/dtos/ChamadoDTO.java
@@ -4,8 +4,12 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
 import org.example.domain.Chamado;
 
+@Getter
+@Setter
 public class ChamadoDTO implements Serializable{
     private static final long serialVersionUID = 1L;
 
@@ -46,94 +50,6 @@ public class ChamadoDTO implements Serializable{
         this.cliente = obj.getCliente().getId();
         this.nomeTecnico = obj.getTecnico().getNome();
         this.nomeCliente = obj.getCliente().getNome();
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public LocalDate getDataAbertura() {
-        return dataAbertura;
-    }
-
-    public void setDataAbertura(LocalDate dataAbertura) {
-        this.dataAbertura = dataAbertura;
-    }
-
-    public LocalDate getDataFechamento() {
-        return dataFechamento;
-    }
-
-    public void setDataFechamento(LocalDate dataFechamento) {
-        this.dataFechamento = dataFechamento;
-    }
-
-    public Integer getPrioridade() {
-        return prioridade;
-    }
-
-    public void setPrioridade(Integer prioridade) {
-        this.prioridade = prioridade;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getObservacoes() {
-        return observacoes;
-    }
-
-    public void setObservacoes(String observacoes) {
-        this.observacoes = observacoes;
-    }
-
-    public Integer getTecnico() {
-        return tecnico;
-    }
-
-    public void setTecnico(Integer tecnico) {
-        this.tecnico = tecnico;
-    }
-
-    public Integer getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Integer cliente) {
-        this.cliente = cliente;
-    }
-
-    public String getNomeTecnico() {
-        return nomeTecnico;
-    }
-
-    public void setNomeTecnico(String nomeTecnico) {
-        this.nomeTecnico = nomeTecnico;
-    }
-
-    public String getNomeCliente() {
-        return nomeCliente;
-    }
-
-    public void setNomeCliente(String nomeCliente) {
-        this.nomeCliente = nomeCliente;
     }
 
 }
--- src/main/java/org/example/domain/dtos/ClienteDTO.java
+++ src/main/java/org/example/domain/dtos/ClienteDTO.java
@@ -7,9 +7,13 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.Setter;
 import org.example.domain.Cliente;
 import org.example.domain.enums.Perfil;
 
+@Getter
+@Setter
 public class ClienteDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
@@ -43,60 +47,12 @@ public class ClienteDTO implements Serializable {
         this.dataCriacao = obj.getDataCriacao();
     }
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
     public Set<Perfil> getPerfis() {
         return perfis.stream().map(x -> Perfil.toEnum(x)).collect(Collectors.toSet());
     }
 
     public void addPerfis(Perfil perfil) {
         this.perfis.add(perfil.getCodigo());
-    }
-
-    public LocalDate getDataCriacao() {
-        return dataCriacao;
-    }
-
-    public void setDataCriacao(LocalDate dataCriacao) {
-        this.dataCriacao = dataCriacao;
     }
 
 }
